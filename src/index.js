a38430bb7b72aeeb88dc370f197a0a19d29701fd
Indisputable09 <sainchyksasha2015999@gmail.com>
2022-06-22 12:36:33 +0300

    Needs to be subdivided into files

--- src/index.js
+++ src/index.js
@@ -6,12 +6,8 @@ const DEBOUNCE_DELAY = 300;
 const SEARCH_URL = 'https://restcountries.com/v3.1/name/';
 const inputEl = document.querySelector('#search-box');
 const listEl = document.querySelector('.country-list');
-const countryInfoEl = document.querySelector('.country-info');
-
 
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
-
-
 
 function fetchCountries(name) {
     const url = `${SEARCH_URL}${name}?fields=name,capital,population,flags,languages`;
@@ -28,23 +24,70 @@ function onInput(e) {
     const inputValue = inputEl.value.trim();
     if (inputValue === '') {
         listEl.innerHTML = '';
-        Notify.warning('blablabla');
         return;
     }
-    fetchCountries(inputValue).then(data => {
+    fetchCountries(inputValue).then(correctMarkup).catch(() => {
+        Notify.failure("Oops, there is no country with that name")
+        listEl.innerHTML = "";
+});
+}
+
+function possibleCountriesMrkup(data) {
+    const markup = data.map(({flags, name}) => {
+        const flagsEl = Object.values(flags.svg).join('');
+        const nameEl = name.official
+        return `<li>
+        <h2 class = "small__text">
+        <span class = "small__img"><img src="${flagsEl}" alt="country flag" width = "30"/></span>${nameEl}
+        </h2></li>`
+    }).join('');
+    listEl.innerHTML = markup;
+}
+
+function chosenCountryMarkup(data) {
+    const markup = data.map(({ languages, flags, name, capital, population }) => {
+        const languagesEl = Object.values(languages).join(", ");
+        const numberOfLanguages = Object.keys(languages);
+        const flagsEl = Object.values(flags.svg).join('');
+        const nameEl = name.official;
+
+        if (numberOfLanguages.length > 1) {
+            return `<li>
+        <h1>
+        <span class = "original__img"><img src="${flagsEl}" alt="country flag" width = "40"/></span>${nameEl}
+        </h1>
+      <p><strong>Capital:</strong> ${capital}</p>
+      <p><strong>Population:</strong> ${population}</p>
+      <p><strong>Languages:</strong> ${languagesEl}</p>
+      </li>`
+            
+        }
+        else {
+            return `<li>
+        <h1>
+        <span class = "original__img"><img src="${flagsEl}" alt="country flag" width = "40"/></span>${nameEl}
+        </h1>
+      <p><strong>Capital:</strong> ${capital}</p>
+      <p><strong>Population:</strong> ${population}</p>
+      <p><strong>Language:</strong> ${languagesEl}</p>
+      </li>`
+        }
+        
+    }).join('');
+    listEl.innerHTML = markup;
+}
+
+function correctMarkup(data) {
+    {
         if (data.length > 10) {
             Notify.info('Too many matches found. Please enter a more specific name.')
+            listEl.innerHTML = '';
         }
         if (data.length >= 2 && data.length <= 10) {
-            Notify.success('good')
+            possibleCountriesMrkup(data);
         }
-    }).catch(console.log);
-}
-
-function possibleCountriesMrkup() {
-    
-}
-
-function chosenCountryMarkup() {
-
+        if (data.length == 1) {
+            chosenCountryMarkup(data);
+        }
+    }
 }
